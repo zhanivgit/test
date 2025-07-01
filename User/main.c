34fcc7ceeeb3e3f38961691484db98c1cdfbd44f
zhaniv <zhanivvv@gmail.com>
2025-07-01 20:04:08 +0800

    第四次提交t

--- User/main.c
+++ User/main.c
@@ -6,21 +6,7 @@
 uint16_t AD0, AD1, AD2, AD3;
 
 int main(void)
-{
 
-	
-	while (1)
-	{
-		AD0 = AD_GetValue(ADC_Channel_0);
-		AD1 = AD_GetValue(ADC_Channel_1);
-		AD2 = AD_GetValue(ADC_Channel_2);
-		AD3 = AD_GetValue(ADC_Channel_3);
-		
-		OLED_ShowNum(1, 5, AD0, 4);
-		OLED_ShowNum(2, 5, AD1, 4);
-		OLED_ShowNum(3, 5, AD2, 4);
-		OLED_ShowNum(4, 5, AD3, 4);
-		
 		Delay_ms(100);
 	}
 }
